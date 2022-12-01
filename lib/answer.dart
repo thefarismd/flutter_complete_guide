1df4c072d141d5e69aeef9b478ad4f04d92f36be
thefarismd <thefarismd@icloud.com>
2022-12-01 10:31:51 +0800

    passing function to widget class

---
+++ lib/answer.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class Answer extends StatelessWidget {
+  // const MyWidget({Key key}) : super(key: key);
+
+  final Function selectHandler;
+
+  Answer(this.selectHandler);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      child: ElevatedButton(
+          onPressed: selectHandler,
+          child: Text('Answer 1'),
+          style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all(Colors.blue))),
+    );
+  }
+}
